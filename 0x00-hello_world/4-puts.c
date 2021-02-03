296f82f98dbd7043c5cf48ee9c50add3d0033530
root <henocknegash86@gmail.com>
2021-02-03 13:22:26 +0000

    up to 5

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *puts function to print strings
+ */
+int main(void)
+{
+char str1[50];
+strcpy(str1, "Programming is like building a multilingual puzzle");
+puts(str1);
+return (0);
+}
