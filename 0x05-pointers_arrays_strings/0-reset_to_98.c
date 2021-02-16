d32ffaf84194006e2589a2f97f1a7de2531efa80
root <henocknegash86@gmail.com>
2021-02-16 08:33:02 +0000

    error fix

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
-#include<holberton.h>
+#include "holberton.h"
 /**
  * reset_to_98 - function to reset the pinters location to 98
- *
- * 
- *
- */
-void reset_to_98(int *n){
-*n=98;	
+*
+*
+*
+*/
+void reset_to_98(int *n)
+{
+*n = 98;
 }
