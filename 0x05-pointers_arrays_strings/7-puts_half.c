2ffd1726208af3aa63906f866405ed0e2ca65f96
root <henocknegash86@gmail.com>
2021-02-16 11:54:02 +0000

    error fix 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,27 +10,14 @@ void puts_half(char *str)
 int i, j = 0;
 
 while (str[i] != '\0')
-{
 i++;
-}
-if (i % 2 == 0)
-{
 j = i / 2;
-while (j < i)
-{
-_putchar(str[j];
+if (i % 2 == 1)
 j++;
-}
-_putchar('\n');
-}
-else
-{
-j = (i - 1) / 2;
 while (j < i)
 {
 _putchar(str[j]);
 j++;
 }
 _putchar('\n');
-}
 }
