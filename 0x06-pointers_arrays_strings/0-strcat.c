0b0e78278a9adfcdbd107889afabe4f10c19f7c3
Henock Negash <HenockN@eslsc.com>
2021-02-18 15:05:37 +0300

    error fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,13 +9,12 @@ char *_strcat(char *dest, char *src)
 {
 int i, j;
 while (dest[i] != '\0')
-{
 i++;
-}
-for (j = 0; src[j] != '\0'; ++j, ++i)
+for (j = 0; src[j] != '\0'; j++)
 {
 dest[i] = src[j];
+i++;
 }
-dest[i] = '\0';
+dest[i] = src[j];
 return (dest);
 }
